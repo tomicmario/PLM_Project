6a99ca6151f5583da7385434583b0610f4263cb3
Janis Chiffelle <janischiffelle@hotmail.ch>
2023-05-17 09:16:42 +0200

    add first movement player

--- src/simple_jframe/swing.clj
+++ src/simple_jframe/swing.clj
@@ -11,9 +11,10 @@
 (def ^:private panel (JPanel.))
 (def ^:private dimension (Dimension. 500 500))
 (def image (BufferedImage. 500 500 BufferedImage/TYPE_INT_RGB))
-(def ^:private inputState (atom #{:x :y}))
+(def ^:private inputState (atom #{}))
 (def ^:private mousePosition (atom {:x 0, :y 0}))
 (get @mousePosition :x)
+(def player (atom {:x 0, :y 0}))
 
 (defn inputList []
   (into [] @inputState))
@@ -25,22 +26,31 @@
 (defn setDirection [fn event]
   (let  [keycode (.getKeyCode event)]
     (if (= keycode KeyEvent/VK_W) (swap! inputState fn :up)
-        nil ) 
+        nil) 
     (if (= keycode KeyEvent/VK_S) (swap! inputState fn :down)
         nil) 
     (if (= keycode KeyEvent/VK_A) (swap! inputState fn :left)
         nil) 
     (if (= keycode KeyEvent/VK_D) (swap! inputState fn :right)
-        nil))
-  )
+        nil)))
+  
 
 (defn handleRelease [event] 
-  (setDirection disj event)
-  )
+  (setDirection disj event))
+  
   
 (defn handlePress [event] 
-  (setDirection conj event)
-  )
+  (setDirection conj event))
+  
+
+(add-watch inputState :watcher
+  (fn [key atom old-state new-state]
+    (println new-state)
+    (if (contains? new-state :down) (swap! player update :y + 1) nil)
+    (if (contains? new-state :up) (swap! player update :y - 1) nil)
+    (if (contains? new-state :left) (swap! player update :x - 1) nil)
+    (if (contains? new-state :right) (swap! player update :x + 1) nil)))
+             
 
 (def mouseListener
   (proxy [MouseInputAdapter] []
@@ -49,16 +59,16 @@
       (swap! mousePosition assoc-in [:y] (.getY m)))
     (mouseDragged [#^MouseEvent m]
       (swap! mousePosition assoc-in [:x] (.getX m))
-      (swap! mousePosition assoc-in [:y] (.getY m))))
-  )
+      (swap! mousePosition assoc-in [:y] (.getY m)))))
+  
 
 (def clickListener 
   (proxy [MouseInputAdapter] []
     (mousePressed [#^MouseEvent m]
       (swap! inputState conj :click))
     (mouseReleased [#^MouseEvent m]
-      (swap! inputState disj :click)))
-  )
+      (swap! inputState disj :click))))
+  
 
 (defn init [title] 
     (doto frame 
@@ -81,31 +91,29 @@
           (.setPreferredSize dimension)
           (.addMouseMotionListener mouseListener)
           (.addMouseListener clickListener)) 
-    (.pack frame)
-    )
+    (.pack frame))
+    
 
 (defn clear []
   (let [graphics (.createGraphics image)]
     (doto ^Graphics2D graphics
       (.setColor Color/WHITE)
-      (.fill (Rectangle2D$Double. 0 0 1000 1000))))
-  )
+      (.fill (Rectangle2D$Double. 0 0 1000 1000)))))
+  
 
 (defn drawRect [x, y] 
   (let [graphics (.createGraphics image)]
     (doto ^Graphics2D graphics
           (.setColor Color/RED)
-          (.fill (Rectangle2D$Double. x y 10 10))))
-  )
+          (.fill (Rectangle2D$Double. x y 10 10)))))
+  
 
 (defn display [] 
   ;(println inputState)
-  (println mousePosition)
-  (let [panelGraphics (.getGraphics panel)
-        x (get @mousePosition :x)
-        y (get @mousePosition :y)] 
-    (drawRect x y)
+  ;(println mousePosition)
+  (let [panelGraphics (.getGraphics panel)] 
+    (drawRect (@player :x) (@player :y))
     (doto ^Graphics2D panelGraphics
-      (.drawImage ^BufferedImage image 0 0 nil)) 
-    )
-  )
+      (.drawImage ^BufferedImage image 0 0 nil)))) 
+    
+  
