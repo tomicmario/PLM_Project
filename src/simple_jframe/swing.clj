5ebb3f287f8b52f3e568e2f112995e162807dfe2
Mario Tomic <mario-tomic@hotmail.com>
2023-05-17 11:15:05 +0200

    basic projectile spawn

--- src/simple_jframe/swing.clj
+++ src/simple_jframe/swing.clj
@@ -2,7 +2,7 @@
   (:import [javax.swing JFrame JPanel])
   (:import [java.awt Color Dimension Graphics2D])
   (:import [java.awt.image BufferedImage])
-  (:import [java.awt.geom Rectangle2D$Double])
+  (:import [java.awt.geom Rectangle2D$Double Ellipse2D$Double])
   (:import [java.awt.event KeyAdapter KeyEvent MouseEvent])
   (:import [javax.swing.event MouseInputAdapter])
   (:require [clojure.core.async :as a :refer [<!! >!! <! >!]]))
@@ -16,6 +16,23 @@
 (get @mousePosition :x)
 (def player (atom {:x 0, :y 0}))
 (def playerVector (atom {:x 0, :y 0}))
+(def projectiles (atom []))
+
+(defn calculate-angle [target-x target-y x y]
+  (Math/atan2 (- target-x x) (- target-y y)))
+
+(defn createProjectile [player mousePosition] 
+  (let [x (:x player)
+        y (:y player)
+        mouse-x (:x mousePosition)
+        mouse-y (:y mousePosition)
+        angle ^Double(calculate-angle mouse-x mouse-y x y)] 
+    {:x x :y y 
+     :vec-x (- (Math/cos angle) (Math/sin angle)) 
+     :vec-y (+ (Math/sin angle) (Math/cos angle))}))
+
+
+
 
 (defn inputList []
   (into [] @inputState))
@@ -39,7 +56,6 @@
 (defn handleRelease [event] 
   (setDirection disj event))
   
-  
 (defn handlePress [event] 
   (setDirection conj event))
   
@@ -49,14 +65,10 @@
     (swap! playerVector assoc-in [:y] y)
     (swap! playerVector assoc-in [:x] x)))
 
-(updateVector)
-
 (defn movePlayer []
   (updateVector)
   (swap! player assoc-in [:y] (+ (:y @playerVector) (:y @player)))
   (swap! player assoc-in [:x] (+ (:x @playerVector) (:x @player))))
-
-(movePlayer)
              
 
 (def mouseListener
@@ -108,17 +120,42 @@
       (.fill (Rectangle2D$Double. 0 0 1000 1000)))))
   
 
+(defn drawCircle [x, y]
+  (let [graphics (.createGraphics image)]
+    (doto ^Graphics2D graphics
+      (.setColor Color/RED)
+      (.fill (Ellipse2D$Double. x y 10 10)))))
+
 (defn drawRect [x, y] 
   (let [graphics (.createGraphics image)]
     (doto ^Graphics2D graphics
           (.setColor Color/RED)
           (.fill (Rectangle2D$Double. x y 10 10)))))
-  
+
+(defn moveProjectile [projectile]
+  {:x (+ (:x projectile) (:vec-x projectile))
+   :y (+ (:y projectile) (:vec-y projectile))
+   :vec-x (:vec-x projectile)
+   :vec-y (:vec-y projectile)})
+
+(defn moveProjectiles []
+  (if (empty? @projectiles) nil
+  (swap! projectiles (fn [proj] (map moveProjectile proj)))))
+
+(defn drawProjectile [projectile]
+  (drawCircle (:x projectile) (:y projectile)))
+
+(defn drawProjectiles []
+  (doseq [proj @projectiles] 
+     (drawProjectile proj)))
 
 (defn display [] 
   ;(println inputState)
   ;(println mousePosition)
-  (let [panelGraphics (.getGraphics panel)] 
+  (let [panelGraphics (.getGraphics panel)]
+    (if (contains? @inputState :click ) (swap! projectiles conj (createProjectile @player @mousePosition)) nil)
+    (moveProjectiles)
+    (drawProjectiles)
     (movePlayer)
     (drawRect (@player :x) (@player :y))
     (doto ^Graphics2D panelGraphics
